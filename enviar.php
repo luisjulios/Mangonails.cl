316bb6af49cf5b86c9d23e0e407c0ce24122c634
Luis Julio <antonio.saurith@gmail.com>
2021-08-17 21:28:27 -0400

    Se arreglan errores del js de wow

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php 
+$destinatario = "antonio.saurith@gmail.com";
+$nombre_apellido = $_POST["nombre_apellido"];
+$email = $_POST["email"];
+$telefono = $_POST["telefono"];
+$mensaje = $_POST["mensaje"];
+
+$contenido = "nombre"
+
+?>
